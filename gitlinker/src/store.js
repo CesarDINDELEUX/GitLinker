12ffc8c5510306169e60e30d516673cb4e92a9a3
CesarDINDELEUX <cesar.dindeleux@gmail.com>
2019-03-06 14:49:20 +0100

    Install Axios and Bootstrap

--- gitlinker/src/store.js
+++ gitlinker/src/store.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(Vuex)
+Vue.use(VueAxios, axios)
+Vue.use(BootstrapVue)
 
 export default new Vuex.Store({
   state: {
