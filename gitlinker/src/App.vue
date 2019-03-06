0024c2c1d698582dc071169d255a9e61042094cf
CesarDINDELEUX <cesar.dindeleux@gmail.com>
2019-03-06 14:22:29 +0100

    TestCommit Cesar

--- gitlinker/src/App.vue
+++ gitlinker/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About22</router-link>
+      <router-link to="/about">About222</router-link>
     </div>
     <router-view/>
   </div>
